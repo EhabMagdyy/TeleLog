564264dda7334c7e8771475d1ad0c86c966aa7c2
EhabMagdyy <ehabmagdy0044@gmail.com>
2026-01-08 05:06:43 +0200

    Added File Source Testing Code with Log & Sink testing code

--- app/main.cpp
+++ app/main.cpp
@@ -9,13 +9,24 @@
 int main(){
     std::cout << "Log Telemetry System" << std::endl;
 
-   FileTelemetrySourceImpl source;
+    // Log & Sink Testing
+    LogManager logMang;
+    logMang.addSink(std::make_unique<ConsoleSink>());
+    logMang.addSink(std::make_unique<FileSink>("app.log"));
 
+    logMang.addLog(LogMessage("InfoApp", "Out of Cotext", "Algeria is the strongest team in AFCON 2025.", LogType::INFO));
+    logMang.addLog(LogMessage("WarnApp", "Reminder", "Inter will take the title.", LogType::WARNING));
+
+    logMang.routeLogsForAllSinks();
+    FileSink fs("err.log");
+    fs.write(LogMessage("ErrorApp", "Context bla bla", "Cristiano Ronaldo is the GOAT.", LogType::ERROR));
+
+    // File Source Testing
+    FileTelemetrySourceImpl source;
     if(!source.openSource()){
         std::cerr << "Failed to open source.txt" << std::endl;
         return 1;
     }
-
     std::string data;
     while(source.readSource(data)){
         std::cout << "Read: " << data << std::endl;
